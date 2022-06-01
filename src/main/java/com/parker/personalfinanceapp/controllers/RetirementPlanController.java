6b7014695ba547e1073500ea9d1d90bd73263bcc
Chris Parker <chris@parkerdesignbuild.com>
2022-05-31 20:36:18 -0400

    retirement plan function

--- src/main/java/com/parker/personalfinanceapp/controllers/RetirementPlanController.java
+++ src/main/java/com/parker/personalfinanceapp/controllers/RetirementPlanController.java
@@ -1,6 +1,7 @@
 package com.parker.personalfinanceapp.controllers;
 
 import com.parker.personalfinanceapp.exceptions.NoSuchRetirementPlanException;
+import com.parker.personalfinanceapp.exceptions.NoSuchUserException;
 import com.parker.personalfinanceapp.models.RetirementPlan;
 import com.parker.personalfinanceapp.models.UserFactory;
 import com.parker.personalfinanceapp.services.RetirementPlanService;
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -24,20 +26,20 @@ public class RetirementPlanController {
     }
 
     @RequestMapping("/view")
-    public String viewRetirementPlan(Model model, Authentication auth) {
+    public String viewRetirementPlan(Model model, Authentication auth) throws NoSuchUserException {
         model.addAttribute("plan", retirementPlanService.getRetirementPlan(UserFactory.createUser(auth)));
         return "retirement-plan-view";
     }
 
     @RequestMapping("/new")
-    public String createRetirementPlan(Model model, Authentication auth, @RequestParam RetirementPlan retirementPlan) {
+    public String createRetirementPlan(Model model, Authentication auth, @ModelAttribute RetirementPlan retirementPlan) throws NoSuchUserException {
         model.addAttribute("plan",
                 retirementPlanService.createRetirementPlan(UserFactory.createUser(auth), retirementPlan));
         return "retirement-plan-view";
     }
 
     @RequestMapping("/edit")
-    public String viewEditRetirementPlanForm(Model model, Authentication auth) {
+    public String viewEditRetirementPlanForm(Model model, Authentication auth) throws NoSuchUserException {
         model.addAttribute("plan",
                 retirementPlanService.getRetirementPlan(UserFactory.createUser(auth)));
         return "retirement-plan-edit";
@@ -45,7 +47,7 @@ public class RetirementPlanController {
 
     @RequestMapping("/update")
     public String updateRetirementPlan(Model model, Authentication auth, @RequestParam RetirementPlan retirementPlan)
-            throws NoSuchRetirementPlanException {
+            throws NoSuchRetirementPlanException, NoSuchUserException {
         model.addAttribute("plan",
                 retirementPlanService.updateRetirementPlan(UserFactory.createUser(auth), retirementPlan));
         return "retirement-plan-view";
@@ -53,7 +55,7 @@ public class RetirementPlanController {
 
     @RequestMapping("/delete")
     public String deleteRetirementPlan(Authentication auth)
-            throws NoSuchRetirementPlanException {
+            throws NoSuchRetirementPlanException, NoSuchUserException {
         retirementPlanService.deleteRetirementPlan(UserFactory.createUser(auth));
         return "redirect:/";
     }
--- src/main/java/com/parker/personalfinanceapp/services/RetirementPlanService.java
+++ src/main/java/com/parker/personalfinanceapp/services/RetirementPlanService.java
@@ -1,12 +1,14 @@
 package com.parker.personalfinanceapp.services;
 
 import com.parker.personalfinanceapp.exceptions.NoSuchRetirementPlanException;
+import com.parker.personalfinanceapp.exceptions.NoSuchUserException;
 import com.parker.personalfinanceapp.models.RetirementPlan;
 import com.parker.personalfinanceapp.models.User;
 import com.parker.personalfinanceapp.repositories.RetirementPlanRepo;
 import com.parker.personalfinanceapp.repositories.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -18,9 +20,16 @@ public class RetirementPlanService {
     @Autowired
     RetirementPlanRepo retirementPlanRepo;
 
-    public RetirementPlan createRetirementPlan(User user, RetirementPlan retirementPlan) {
-        addRetirementPlanToUser(user, retirementPlan);
-        return retirementPlanRepo.save(retirementPlan);
+    @Transactional
+    public RetirementPlan createRetirementPlan(User user, RetirementPlan retirementPlan) throws NoSuchUserException {
+        Optional<User> userOptional = userRepo.findById(user.getId());
+        if (userOptional.isPresent()) {
+            user = userOptional.get();
+            addRetirementPlanToUser(user, retirementPlan);
+            return retirementPlanRepo.save(retirementPlan);
+        } else {
+            throw new NoSuchUserException("User not found.");
+        }
     }
 
     private void addRetirementPlanToUser(User user, RetirementPlan retirementPlan) {
@@ -34,21 +43,41 @@ public class RetirementPlanService {
         return true;
     }
 
-    public RetirementPlan getRetirementPlan(User user) {
-        return user.getRetirementPlan();
+    public RetirementPlan getRetirementPlan(User user) throws NoSuchUserException {
+        Optional<User> userOptional = userRepo.findById(user.getId());
+        if (userOptional.isPresent()) {
+            user = userOptional.get();
+            return user.getRetirementPlan();
+        } else {
+            throw new NoSuchUserException("User not found.");
+        }
     }
 
+    @Transactional
     public RetirementPlan updateRetirementPlan(User user, RetirementPlan retirementPlan)
-            throws NoSuchRetirementPlanException {
+            throws NoSuchRetirementPlanException, NoSuchUserException {
         retirementPlanRepo.delete(getRetirementPlanFromDB(user));
-        addRetirementPlanToUser(user, retirementPlan);
-        return retirementPlanRepo.save(retirementPlan);
+        Optional<User> userOptional = userRepo.findById(user.getId());
+        if (userOptional.isPresent()) {
+            user = userOptional.get();
+            addRetirementPlanToUser(user, retirementPlan);
+            return retirementPlanRepo.save(retirementPlan);
+        } else {
+            throw new NoSuchUserException("User not found.");
+        }
     }
 
-    public void deleteRetirementPlan(User user) throws NoSuchRetirementPlanException {
+    public void deleteRetirementPlan(User user) throws NoSuchRetirementPlanException, NoSuchUserException {
         retirementPlanRepo.delete(getRetirementPlanFromDB(user));
-        user.setRetirementPlan(null);
-        userRepo.save(user);
+        Optional<User> userOptional = userRepo.findById(user.getId());
+        if (userOptional.isPresent()) {
+            user = userOptional.get();
+            user.setRetirementPlan(null);
+            userRepo.save(user);
+        } else {
+            throw new NoSuchUserException("User not found.");
+        }
+
     }
 
     private RetirementPlan getRetirementPlanFromDB(User user) throws NoSuchRetirementPlanException {
